2dca70ebc61a85657b046887d34b98324068209a
Algo-Goer <45790987+Algo-Goer@users.noreply.github.com>
2021-12-21 10:17:47 +0800

    Add files via upload

---
+++ LeetCode C/1221 1154.一年中的第几天 Easy.c
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=c
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    scanf("%d %d", &nums[numsSize], &target);
+    for (int i = 0; i < numsSize; i++)
+    {
+        int j = target - nums[i];
+        if (j )
+    }
+
+}
+// @lc code=end
+
